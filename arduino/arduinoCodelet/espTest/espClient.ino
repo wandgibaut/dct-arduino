5bbd18daf049ebc5c9bec616f096c202ae11a147
Wand Gibaut <wand@me>
2026-04-14 21:38:00 -0300

    separating arduino from python

---
+++ arduino/arduinoCodelet/espTest/espClient.ino
@@ -0,0 +1,111 @@
+/******************************************************************************#
+# Copyright (c) 2020  Wandemberg Gibaut                                       #
+# All rights reserved. This program and the accompanying materials            #
+# are made available under the terms of the GNU Lesser Public License v3      #
+# which accompanies this distribution, and is available at                    #
+# http://www.gnu.org/licenses/lgpl.html                                       #
+#                                                                             #
+# Contributors:                                                               #
+#      W. Gibaut                                                              #
+#                                                                             #
+#******************************************************************************/
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "your-ssid";
+const char* password = "your-password";
+
+const char* host = "<your host>";
+const uint16_t port = 9999; //change
+
+void setup() {
+  Serial.begin(115200);
+
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  Serial.print("connecting to ");
+  Serial.print(host);
+  Serial.print(':');
+  Serial.println(port);
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  if (!client.connect(host, port)) {
+    Serial.println("connection failed");
+    delay(5000);
+    return;
+  }
+
+  // This will send a string to the server
+  Serial.println("sending data to server");
+  if (client.connected()) {
+    client.println("get_perceptual-memory");
+  }
+
+  // wait for data to be available
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      delay(60000);
+      return;
+    }
+  }
+
+  // Read all the lines of the reply from server and print them to Serial
+  Serial.println("receiving from remote server");
+  
+  DynamicJsonDocument doc(1024);
+
+  // Parse JSON object
+  DeserializationError error = deserializeJson(doc, client);
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.c_str());
+    return;
+  }
+
+  // Extract values
+  Serial.println(F("Response:"));
+  
+  serializeJson(doc, Serial);
+  int I = doc["I"].as<int>() +1;
+  
+  if (!client.connect(host, port)) {
+    Serial.println("connection failed");
+    delay(5000);
+    return;
+  }
+  
+  client.println("set_perceptual-memory_I_" + String(I));
+
+  // Close the connection
+  Serial.println();
+  Serial.println("closing connection");
+  client.stop();
+
+  delay(500); // execute once every  0.5 seconds, don't flood remote service
+}
